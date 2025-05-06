a7b65fd83bbaf2232add5ad88a79fcfadad6d0ca
abinashpradhan01 <teamap13@outlook.com>
2025-05-06 14:23:49 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import ElasticNetCV
+import pickle
+
+application = Flask(__name__)
+app=application
+
+## import elasticcv and standardscaler pickle
+loaded_scaler = pickle.load(open("models/scaler.pkl", "rb"))
+loaded_model = pickle.load(open("models/elasticcv_model.pkl", "rb"))
+
+
+# home page
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method=='POST':
+        Temperature=float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        new_data_scaled=loaded_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=loaded_model.predict(new_data_scaled)
+
+        return render_template('home.html',result=result[0])
+
+    else:
+        return render_template('home.html')
+
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
